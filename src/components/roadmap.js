cfed0c726f8ed3a34b79d2a0f0cbc6a50f99d5c6
brainartfu <brainartfu@gmail.com>
2023-04-02 15:47:43 +0300

    roadmap update

--- src/components/roadmap.js
+++ src/components/roadmap.js
@@ -12,7 +12,7 @@ const Roadmap = () => {
           <div className="container left">
             <div className="content">
               <h2>Blockchain</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>Our team is in charge of various blockchain developments such as various NFT releases, blockchain bot development, and commercial transaction development.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -22,7 +22,7 @@ const Roadmap = () => {
           <div className="container right">
             <div className="content">
               <h2>Mean Stack</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>Our team is mainly responsible for web and mobile responsive clean design and development of various web and mobile applications using simple projects to massive mean-stack.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -32,7 +32,7 @@ const Roadmap = () => {
           <div className="container left">
             <div className="content">
               <h2>Mern Stack</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>Our team is mainly responsible for web and mobile responsive clean design and development of various web and mobile applications using simple projects to massive mean-stack.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -42,7 +42,7 @@ const Roadmap = () => {
           <div className="container right">
             <div className="content">
               <h2>Laravel</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>Since 2016, Our team in charge of web design and site development using various php frameworks like laravel, wordpress, woo-commerce.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -52,7 +52,7 @@ const Roadmap = () => {
           <div className="container left">
             <div className="content">
               <h2>Android and IOS</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>Our team in charge of development Android and IOS app application using various react native and Xcode tools. We have various app applications to make life easier in various fields and are also in charge of consulting for them.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -62,7 +62,7 @@ const Roadmap = () => {
           <div className="container right view-before">
             <div className="content">
               <h2>Deploy and release</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>There is also a department in charge of Android and IOS app launch and web hosting. Our team is responsible for rapid and expansive app and web application launches while maintaining close ties with multiple hosting providers.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -72,7 +72,7 @@ const Roadmap = () => {
           <div className="container left">
             <div className="content">
               <h2>OPEN AI</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p>It is a fact that everyone knows that Open AI is now becoming a trend in view genesis and development, and Our team has been exploring and developing this since 2021, and product development in various fields of Open AI, such as Chat GPT 3.5 model training and image generation. We are in the process of researching and developing Chat GPT4.0.</p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
@@ -82,7 +82,7 @@ const Roadmap = () => {
           <div className="container right view-before">
             <div className="content">
               <h2>IOT</h2>
-              <p>We have vast experience from version 3 to the latest version 14 of Angular,  have highly skilled Angular developers that can develop and release successful and fast.  If you want Angular in your application, you can always contact us.</p>
+              <p></p>
               <div className="touchBtnGroup">
                 <button className="btn touchBtn">Projects we have</button>
                 <button className="btn touchBtn">Get in touch</button>
