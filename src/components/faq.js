0dca40ee6082fa19959af347a0e8afaf97ecde5e
DigitalAgench711 <roman@directnorth.digital>
2023-04-02 15:52:04 +0300

    roadmap update

---
+++ src/components/faq.js
@@ -0,0 +1,181 @@
+import { useState } from "react"
+
+import sheep from "../img/sheep.png"
+import rabbit from "../img/faq-rabbit.png"
+import carrot_rabbit from "../img/carrot-rabbit.png"
+import rab_fox from "../img/rabi-fox.png"
+import lion from "../img/lion.png"
+import buffelo from "../img/buffelo.png"
+import ox from "../img/ox.png"
+import sloth from "../img/sloth.png"
+import furry from "../img/furry.png"
+import otter from "../img/otter.png"
+import mr from "../img/mr.png"
+import fox from "../img/fox.png"
+import "../styles/faq.css"
+
+const Faq = () => {
+  const [mintprice, setMintprice] = useState(false)
+  const [mintdate, setMintdate] = useState(false)
+  const [mintcount, setMintcount] = useState(false)
+  const [presale, setPresale] = useState(false)
+  const [benefit, setBenefit] = useState(false)
+  const [reveal, setReveal] = useState(false)
+
+  const mintPrice = () => {
+    setMintprice(!mintprice)
+  }
+  const mintDate = () => {
+    setMintdate(!mintdate)
+  }
+  const mintCount = () => {
+    setMintcount(!mintcount)
+  }
+  const Presale = () => {
+    setPresale(!presale)
+  }
+  const Benefit = () => {
+    setBenefit(!benefit)
+  }
+  const Reveal = () => {
+    setReveal(!reveal)
+  }
+
+  return (
+    <div className="faq">
+      <div className="faq-title">
+        <h2 className="f-orange">F A Q</h2>
+        <p>FAQ</p>
+        <div className="small-line" />
+      </div>
+      <div className="faq-bar-title">
+        <p>Frequently Asked</p>
+        <p>Questions</p>
+      </div>
+      <div className="faq-elements">
+        <div className="faq-element">
+          <div className="faq-question" onClick={mintPrice}>
+            <p>Your team have Marking and consultanting? or Only development Team?</p>
+            <button className={mintprice ? "" : "hide"}>-</button>
+            <button className={mintprice ? "hide" : ""}>+</button>
+          </div>
+          <div className={mintprice ? "faq-answer" : "hide"}>
+            <p>Our team has good experience of Marketing and are response to marking with development.</p>
+            <p>Also We have great idea and serveral products to make life easier</p>
+          </div>
+        </div>
+        <div className="faq-element">
+          <div className="faq-question" onClick={mintDate}>
+            <p>What stack does your team mainly develop?</p>
+            <button className={mintdate ? "" : "hide"}>-</button>
+            <button className={mintdate ? "hide" : ""}>+</button>
+          </div>
+          <div className={mintdate ? "faq-answer" : "hide"}>
+            <p>We have great team can develop with Angular, React, Next.js at Frontend</p>
+            <p>We are good at Nest.js, Express, MongoDB, Mongoose at Backend</p>
+            <p>Also React Native team for mobile application and Great team at blockchain field</p>
+          </div>
+        </div>
+        <div className="faq-element">
+          <div className="faq-question" onClick={mintCount}>
+            <p>Where your team based on? I want to make team at my region</p>
+            <button className={mintcount ? "" : "hide"}>-</button>
+            <button className={mintcount ? "hide" : ""}>+</button>
+          </div>
+          <div className={mintcount ? "faq-answer" : "hide"}>
+            <p>As we are global team, Our team guys are in all the world. If possible, you can build a great development team in your area.</p>
+            <p>-2st Round of Whitelist Sales: 22MATIC (35 USDT)</p>
+            <p>-Pubic Sales: 30 MATIC (50 USDT)</p>
+          </div>
+        </div>
+        <div className="faq-element">
+          <div className="faq-question" onClick={Presale}>
+            <p>Will there be a pre-sale?</p>
+            <button className={presale ? "" : "hide"}>-</button>
+            <button className={presale ? "hide" : ""}>+</button>
+          </div>
+          <div className={presale ? "faq-answer" : "hide"}>
+            <p>-1st Round of Whitelist Sales: 14MATIC (20 USDT)</p>
+            <p>-2st Round of Whitelist Sales: 22MATIC (35 USDT)</p>
+            <p>-Pubic Sales: 30 MATIC (50 USDT)</p>
+          </div>
+        </div>
+        <div className="faq-element">
+          <div className="faq-question" onClick={Benefit}>
+            <p>What are the benefits of holding one or multiple GHOG NFTs?</p>
+            <button className={benefit ? "" : "hide"}>-</button>
+            <button className={benefit ? "hide" : ""}>+</button>
+          </div>
+          <div className={benefit ? "faq-answer" : "hide"}>
+            <p>-1st Round of Whitelist Sales: 14MATIC (20 USDT)</p>
+            <p>-2st Round of Whitelist Sales: 22MATIC (35 USDT)</p>
+            <p>-Pubic Sales: 30 MATIC (50 USDT)</p>
+          </div>
+        </div>
+        <div className="faq-element">
+          <div className="faq-question" onClick={Reveal}>
+            <p>When is the reveal?</p>
+            <button className={reveal ? "" : "hide"}>-</button>
+            <button className={reveal ? "hide" : ""}>+</button>
+          </div>
+          <div className={reveal ? "faq-answer" : "hide"}>
+            <p>-1st Round of Whitelist Sales: 14MATIC (20 USDT)</p>
+            <p>-2st Round of Whitelist Sales: 22MATIC (35 USDT)</p>
+            <p>-Pubic Sales: 30 MATIC (50 USDT)</p>
+          </div>
+        </div>
+        <div className="faq-sheep ">
+          <img src={sheep} className="f-size" />
+        </div>
+        <div className="faq-rabbit">
+          <img src={rabbit} className="f-size" />
+        </div>
+      </div>
+      <div className="blend faq-left" />
+      <div className="img-gallerys">
+        <div className="img-gallery-top">
+          <div className="element-img">
+            <img src={carrot_rabbit} />
+          </div>
+          <div className="element-img">
+            <img src={rabbit} />
+          </div>
+          <div className="element-img">
+            <img src={rab_fox} />
+          </div>
+          <div className="element-img">
+            <img src={lion} />
+          </div>
+          <div className="element-img">
+            <img src={buffelo} />
+          </div>
+          <div className="element-img">
+            <img src={sheep} />
+          </div>
+        </div>
+        <div className="img-gallery-bottom">
+          <div className="element-img">
+            <img src={ox} />
+          </div>
+          <div className="element-img">
+            <img src={sloth} />
+          </div>
+          <div className="element-img">
+            <img src={furry} />
+          </div>
+          <div className="element-img">
+            <img src={otter} />
+          </div>
+          <div className="element-img">
+            <img src={mr} />
+          </div>
+          <div className="element-img">
+            <img src={fox} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Faq
---
+++ src/components/roadmap.js
@@ -0,0 +1,94 @@
+import "../styles/roadmap.css"
+
+const Roadmap = () => {
+  return (
+    <div className="roadmap">
+      <div className="roadmap-title">
+        <h2 className="f-orange">STACKS WE HAVE</h2>
+        <div className="small-line"></div>
+      </div>
+      <div className="timeline-board">
+        <div className="timeline">
+          <div className="container left">
+            <div className="content">
+              <h2>Blockchain</h2>
+              <p>Our team is in charge of various blockchain developments such as various NFT releases, blockchain bot development, and commercial transaction development.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container right">
+            <div className="content">
+              <h2>Mean Stack</h2>
+              <p>Our team is mainly responsible for web and mobile responsive clean design and development of various web and mobile applications using simple projects to massive mean-stack.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container left">
+            <div className="content">
+              <h2>Mern Stack</h2>
+              <p>Our team is mainly responsible for web and mobile responsive clean design and development of various web and mobile applications using simple projects to massive mean-stack.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container right">
+            <div className="content">
+              <h2>Laravel</h2>
+              <p>Since 2016, Our team in charge of web design and site development using various php frameworks like laravel, wordpress, woo-commerce.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container left">
+            <div className="content">
+              <h2>Android and IOS</h2>
+              <p>Our team in charge of development Android and IOS app application using various react native and Xcode tools. We have various app applications to make life easier in various fields and are also in charge of consulting for them.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container right view-before">
+            <div className="content">
+              <h2>Deploy and release</h2>
+              <p>There is also a department in charge of Android and IOS app launch and web hosting. Our team is responsible for rapid and expansive app and web application launches while maintaining close ties with multiple hosting providers.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <div className="container left">
+            <div className="content">
+              <h2>OPEN AI</h2>
+              <p>It is a fact that everyone knows that Open AI is now becoming a trend in view genesis and development, and Our team has been exploring and developing this since 2021, and product development in various fields of Open AI, such as Chat GPT 3.5 model training and image generation. We are in the process of researching and developing Chat GPT4.0.</p>
+              <div className="touchBtnGroup">
+                <button className="btn touchBtn">Projects we have</button>
+                <button className="btn touchBtn">Get in touch</button>
+              </div>
+            </div>
+          </div>
+          <a className="view" href="#">
+            View All
+          </a>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Roadmap
